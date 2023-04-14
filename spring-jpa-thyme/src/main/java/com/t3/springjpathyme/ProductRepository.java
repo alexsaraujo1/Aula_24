19901230c58d325cebe7705514483a178cb8ada9
alexsaraujo1 <82852965+alexsaraujo1@users.noreply.github.com>
2023-04-13 21:59:03 -0300

    Código feito durante a  aula

---
+++ spring-jpa-thyme/src/main/java/com/t3/springjpathyme/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.t3.springjpathyme;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long>{
+    
+}
+
+ 
+    
+
